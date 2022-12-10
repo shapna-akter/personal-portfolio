680b7b24eb366669161f900f6192df354c9d2d91
Shapna <shapna088@gmail.com>
2022-12-10 17:13:54 +0600

    img added

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import image1 from '../../assets/projectsImage/project1.jpg'
+import image2 from '../../assets/projectsImage/project2.jpg'
+import image3 from '../../assets/projectsImage/project3.jpg'
+import image4 from '../../assets/projectsImage/project4.jpg'
+import image5 from '../../assets/projectsImage/project5.jpg'
+import image6 from '../../assets/projectsImage/project6.png'
 
 const Portfolio = () => {
     return (
